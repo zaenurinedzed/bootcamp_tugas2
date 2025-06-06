e5e025934832a4e66736f7d7dc1a215111faa1af
Zaenurinedzed <zaenuri.tugas@gmail.com>
2025-06-06 22:02:32 +0700

    Add color to the text in HomePage widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Halaman Utama', style: TextStyle(fontSize: 24)),
+      child: Text('Halaman Utama', style: TextStyle(fontSize: 24, color: Colors.blue)),
     );
   }
 }
